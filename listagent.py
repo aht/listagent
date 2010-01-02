d56396eb14056714e9b3fcffa3229d5d9a907fca
Anh Hai Trinh <anh.hai.trinh@gmail.com>
2010-01-03 03:21:24 +0700

    Rewrite listagent using .align()

--- listagent.py
+++ listagent.py
@@ -30,6 +30,7 @@ Listagents offer live view of the original::
   >>> list(a)
   [1, 3, 5]
   >>> x += [8, 9, 10]
+	>>> a.align()
   >>> list(a)
   [1, 3, 5, 7, 9]
 
@@ -43,7 +44,7 @@ Slice assignment also works::
 
 You can sort and reverse the agent, which will mutate the original list
 in-place.  Reversing is as fast as list.reverse.  The sort algorithm is Shell
-sort and is slower than list.sort (which is timsort)::
+sort::
 
   >>> x = [22, 7, 2, -5, 8, 4]
   >>> a = listagent(x)
@@ -71,27 +72,30 @@ Some test cases::
 import collections
 
 
-def idx_translator(start, stop, step):
-	return lambda i: start + i * step
-
-
 class listagent(collections.MutableSequence):
 	def __init__(self, list, slice=slice(None)):
 		self.list = list
 		self.slice = slice
+		self.align()
+
+	def align(self):
+		"""Align the agent to the length of the underlying list"""
+		start, stop, step = self.slice.indices(len(self.list))
+		self.translate = lambda i: start + i * step
+		if step > 0:
+			self.len = int((stop - start - 1) / float(step)) + 1
+		else:
+			self.len = int((stop - start) / float(step))
 
 	def __len__(self):
-		start, stop, step = self.slice.indices(len(self.list))
-		if step > 0:
-			return int((stop - start - 1) / float(step)) + 1
-		else:
-			return int((stop - start) / float(step))
+		return self.len
 
 	def __iter__(self):
-		t = idx_translator(*self.slice.indices(len(self.list)))
 		def iterate():
+			x = self.list
+			t = self.translate
 			for i in range(len(self)):
-				yield self.list[t(i)]
+				yield x[t(i)]
 		return iterate()
 
 	def __getitem__(self, key):
@@ -104,56 +108,57 @@ class listagent(collections.MutableSequence):
 			else:
 				return listagent(self, key)
 		elif type(key) is type(1):
-			t = idx_translator(*self.slice.indices(len(self.list)))
-			return self.list[t(key)]
+			return self.list[self.translate(key)]
 		else:
 			raise TypeError
 
 	def __setitem__(self, key, value):
-		t = idx_translator(*self.slice.indices(len(self.list)))
 		if type(key) is type(1):
-			self.list[t(key)] = value
+			self.list[self.translate(key)] = value
 		elif type(key) is type(slice(1)):
-			v = iter(value)
+			x = self.list
+			t = self.translate
 			for i in range(len(self)):
 				try:
-					self.list[t(i)] = next(v)
-				except StopIteration:
+					x[t(i)] = value[i]
+				except IndexError:
 					raise ValueError("length mismatch")
 		else:
 			raise TypeError
 	
 	def __delitem__(self, key):
 		if type(key) is type(1):
-			t = idx_translator(*self.slice.indices(len(self.list)))
-			del self.list[t(key)]
+			del self.list[self.translate(key)]
+		else:
+		    raise TypeError
 	
 	def insert(self, i, value):
-		t = idx_translator(*self.slice.indices(len(self.list)))
-		self.list.insert(t(i), value)
+		self.list.insert(self.translate(i), value)
 
 	def reverse(self):
-		t = idx_translator(*self.slice.indices(len(self.list)))
-		n = len(self)
-		for k in range(n // 2):
-			i, j = t(k), t((-k-1) % n)
-			self.list[i], self.list[j] = self.list[j], self.list[i]
+		x = self.list
+		t = self.translate
+		i, j = 0, len(self)-1
+		while i <= j:
+			x[t(i)], x[t(j)] = x[t(j)], x[t(i)]
+			i += 1
+			j -= 1
 
 	def sort(self):
 		## Shell sort ##
+		x = self.list
 		n = len(self)
-		translator = idx_translator(*self.slice.indices(len(self.list)))
-		t = map(translator, range(n))
+		t = map(self.translate, range(n))
 		gap = n // 2
 		while gap:
 			for k in range(n):
 				i, j = t[k], t[k-gap]
-				tmp = self.list[i]
-				while k >= gap and self.list[j] > tmp:
-					self.list[i] = self.list[j]
+				tmp = x[i]
+				while k >= gap and x[j] > tmp:
+					x[i] = x[j]
 					k -= gap
 					i, j = t[k], t[k-gap]
-				self.list[i] = tmp
+				x[i] = tmp
 			gap = 1 if gap == 2 else int(gap * 5.0 / 11)
 
 	def __repr__(self):
@@ -163,20 +168,35 @@ class listagent(collections.MutableSequence):
 
 
 def permute_next(a):
-    """Transform the given mutable sequence into its next lexicographical
-    permutation.  Return True if that permutation exists, else False.
-    """
-    i = len(a) - 2
-    while i >= 0 and a[i+1] <= a[i]:
-        i -= 1
-    if i < 0:
-        # a is monotonically decreasing
-        return False
-    else:
-        # a[i+1:] is monotonically decreasing
-        j = len(a) - 1
-        while a[j] <= a[i]:
-            j -= 1
-        a[i], a[j] = a[j], a[i]
-        listagent(a)[i+1:].reverse()
-        return True
+	"""Transform the given mutable sequence into its next lexicographical
+	permutation.  Return True if that permutation exists, else False.
+
+	>>> x = [4, 5, 3, 2 ,1]
+	>>> permute_next(x)
+	True
+	>>> x
+	[5, 1, 2, 3, 4]
+	
+	>>> y = [3, 3, 4, 2, 1]
+	>>> permute_next(y);
+	True
+	>>> y
+	[3, 4, 1, 2, 3]
+	
+	>>> permute_next([5, 4, 2, 0])
+	False
+	"""
+	i = len(a) - 2
+	while i >= 0 and a[i+1] <= a[i]:
+		i -= 1
+	if i < 0:
+		# a is monotonically decreasing
+		return False
+	else:
+		# a[i+1:] is monotonically decreasing
+		j = len(a) - 1
+		while a[j] <= a[i]:
+			j -= 1
+		a[i], a[j] = a[j], a[i]
+		listagent(a)[i+1:].reverse()
+		return True
